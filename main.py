14e123064162f4c7bce9eb00feda2c55f588ce14
Thales <thales.henriquesdias@gmail.com>
2025-07-21 19:20:25 -0300

    Tipos de tratamento de erro

---
+++ main.py
@@ -0,0 +1,33 @@
+# Exemplo que causa TypeError
+#try:
+#    resultado = len(5)
+#except TypeError as e:
+#    print(e)  # Imprime a mensagem de erro
+
+
+#numero = 10
+#if isinstance(numero, int):
+#    print("A variável é um inteiro.")
+#else:
+#    print("A variável não é um inteiro.")
+
+#numero_inteiro = 5
+#numero_flutuante = 2.5
+## Converte o inteiro para flutuante e realiza a soma
+#soma = float(numero_inteiro) + numero_flutuante
+#print(soma)  # Resultado: 7.5
+
+#try:
+#    # Código que pode gerar uma exceção
+#    resultado = 10 / 0
+#except ZeroDivisionError:
+#    # Código que executa se a exceção ZeroDivisionError for levantada
+#    print("Divisão por zero não é permitida.")
+
+#idade = 20
+#if idade < 18:
+#    print("Menor de idade")
+#elif idade == 18:
+#    print("Exatamente 18 anos")
+#else:
+#    print("Maior de idade")
